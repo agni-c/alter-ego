1813980b847d968fda89321a862c5839b5bb5076
phantom0z <agnibha0016@gmail.com>
2023-08-09 13:12:20 +0530

    implemented search input ui and debounce

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,13 @@
-import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
+import SearchInput from '@/components/search-input';
+
 const RootPage = () => {
-	return <div></div>;
+	return (
+		<div className='h-full p-4 space-y-2'>
+			<SearchInput />
+		</div>
+	);
 };
 
 export default RootPage;
